6b67dc65b26c0b5b03f10e492e45faedd0cb00a5
Asharib Ali <96954816+Ashrib@users.noreply.github.com>
2022-08-14 16:08:54 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
-        // Ch#1 (alerts)
+        // -----Ch#1 (alerts)------
 //alert("Thanks for your input");
 
         // Ch#2 (Variables for Strings)
 // var thanx = "Thanks for your input!";
 // alert(thanx);
 
-        // Ch#3 (Variables for Numbers)
- 
+        // -------Ch#3 (Variables for Numbers)-------
 // var originalNum = 23;
 // var newNum = originalNum + 7;
 // console.log(newNum);
@@ -24,27 +23,27 @@
 // var newNum = originalNum + 7;
 // console.log(newNum);
 
-        // Ch#8 (Concatenating text strings)
+        // ------Ch#8 (Concatenating text strings)------
 // var message = "Thanks, ";
 // var userName = "Susan";
 // var banger = "!";
 // var customMess = message + userName + banger;
 // alert(customMess);
 
-        // Ch#9 (Prompt)
+        // -------Ch#9 (Prompt)-------
 // var question = "Your species?";
 // var defaultAnswer = "human";
 // var spec = prompt(question, defaultAnswer);
 // console.log(spec);
 
-        // Ch#10 (if statements)
+        //------ Ch#10 (if statements)------
 // var x = prompt("Where does the Pope live?");
 // var correctAnswer = "Vatican";
 // if (x === correctAnswer) {
 //     alert("Correct!");
 // }
 
-       // Ch#12 (if...else and else if statements) 
+       // ------Ch#12 (if...else and else if statements)------- 
 // var x = prompt("Where does the Pope live?");
 // var correctAnswer = "Vatican";
 // if (x === correctAnswer) {
@@ -57,18 +56,58 @@
 //   alert("Incorrect");
 // }
 
-       // Ch#13 (Testing sets of conditions) 
-var SAT = 40;
-var GPA = 3;
-var avg = 30;
-sport = "circket"; 
-if (SAT > avg || GPA > 2.5 || sport === "football") {
- alert("Welcome to Bubba State!");
-}
-else {
- alert("Have you looked into appliance repair?");
-}
-        // Ch#14 (Testing sets of conditions) 
-if (age > 65 || (age < 21 && res === "U.S.")) {
-        alert()
-}
+       // ------Ch#13 (Testing sets of conditions) ------
+// var weight = 400;
+// var time = 7;
+// var age = 18;
+// var gender = "male";
+// if (weight > 300 && time < 6 && age > 17 && gender === "male") {
+//  alert("Come to our tryout!");
+// }
+// else {
+//  alert("Come to our cookout!");
+// };
+
+// var SAT = 40;
+// var GPA = 3;
+// var avg = 30;
+// sport = "circket"; 
+// if (SAT > avg || GPA > 2.5 || sport === "football") {
+//  alert("Welcome to Bubba State!");
+// }
+// else {
+//  alert("Have you looked into appliance repair?");
+// }
+
+        // ------Ch#14 (Testing sets of conditions)------ 
+// var age = 19;
+// var res = "U.S."
+// if (age > 65 || (age < 21 && res === "U.S.")) {
+//         alert("ok")
+// }
+// else{
+//         alert("no")
+// }
+
+       // ------Ch#14 (if statements nested) ------
+// var c = 10;
+// var d = 10;
+// var x = 2;
+// var y = 2;
+// var h = "ok";
+// var g; 
+// if (c === d) {
+//   if (x === y) {
+//     g = h;
+//     alert(h)
+//   }
+//   else if (a === b) {
+//     g = h;
+//   }
+//   else {
+//     e = f;
+//   }
+// }
+// else {
+//   e = f;
+// }
