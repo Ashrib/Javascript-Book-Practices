0fedc99d53d2b4c28cdf33ffef33fefbcbabd772
Asharib Ali <96954816+Ashrib@users.noreply.github.com>
2022-08-14 15:41:25 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,74 @@
+        // Ch#1 (alerts)
+//alert("Thanks for your input");
+
+        // Ch#2 (Variables for Strings)
+// var thanx = "Thanks for your input!";
+// alert(thanx);
+
+        // Ch#3 (Variables for Numbers)
+ 
+// var originalNum = 23;
+// var newNum = originalNum + 7;
+// console.log(newNum);
+ 
+// var originalNum = 23;
+// var numToBeAdded = 7;
+// var newNum = originalNum + numToBeAdded;
+// console.log(newNum);
+ 
+// var originalNum = 90;
+// originalNum = originalNum + 10;
+// console.log(originalNum);
+ 
+// var originalNum = "23";
+// var newNum = originalNum + 7;
+// console.log(newNum);
+
+        // Ch#8 (Concatenating text strings)
+// var message = "Thanks, ";
+// var userName = "Susan";
+// var banger = "!";
+// var customMess = message + userName + banger;
+// alert(customMess);
+
+        // Ch#9 (Prompt)
+// var question = "Your species?";
+// var defaultAnswer = "human";
+// var spec = prompt(question, defaultAnswer);
+// console.log(spec);
+
+        // Ch#10 (if statements)
+// var x = prompt("Where does the Pope live?");
+// var correctAnswer = "Vatican";
+// if (x === correctAnswer) {
+//     alert("Correct!");
+// }
+
+       // Ch#12 (if...else and else if statements) 
+// var x = prompt("Where does the Pope live?");
+// var correctAnswer = "Vatican";
+// if (x === correctAnswer) {
+//   alert("Correct!");
+// }
+// else if (x === "Rome") {
+//   alert("Incorrect but close");
+// }
+// else {
+//   alert("Incorrect");
+// }
+
+       // Ch#13 (Testing sets of conditions) 
+var SAT = 40;
+var GPA = 3;
+var avg = 30;
+sport = "circket"; 
+if (SAT > avg || GPA > 2.5 || sport === "football") {
+ alert("Welcome to Bubba State!");
+}
+else {
+ alert("Have you looked into appliance repair?");
+}
+        // Ch#14 (Testing sets of conditions) 
+if (age > 65 || (age < 21 && res === "U.S.")) {
+        alert()
+}
